0eab78fd297b79134fb0fdf2cd4157064428de42
mickylyu <mickyl2025@student.cis.edu.hk>
2023-02-14 15:01:33 +0800

    idshlf

--- src/Dog.java
+++ src/Dog.java
@@ -1,2 +1,51 @@
 public class Dog {
+    private int age;
+    private String name;
+    private String color;
+
+    public Dog(int age, String name, String color) {
+        this.age = age;
+        this.name = name;
+        this.color = color;
+    }
+
+    public int getAge(){
+        return age;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public String getColor(){
+        return color;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String setName(String name) {
+        this.name = name;
+        return name;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    private void addOneAge(){
+        age++;
+    }
+
+    public int birthday(){
+        addOneAge();
+        return age;
+    }
+
+    public String toString(){
+        return "The dog is named " + name + " and is " + age + " years old. This dog is " + color + ".";
+    }
+    // "The dog is named Marlo and is 5 years old. This dog is black."
+}
 }
--- src/DogMaker.java
+++ src/DogMaker.java
@@ -1,8 +1,13 @@
 public class DogMaker {
   
   public static void main(String[] args) {
-    
-    
+    Dog dog = new Dog(2, "Jill", "Black and Tan");
+    String jessie = dog.setName("Jessie");
+    System.out.println(dog.getAge());
+    System.out.println(dog.birthday());
+    System.out.println(dog);
   }
-  
+    
 }
+  
+
